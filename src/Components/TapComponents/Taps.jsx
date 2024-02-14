13d27ef53c682d43e5b59f4ec78235d591840c11
kumA0taku <varinpimda@gmail.com>
2024-02-14 15:16:50 +0700

    remove tab and unfinish

--- src/Components/TapComponents/Taps.jsx
+++ src/Components/TapComponents/Taps.jsx
@@ -1,23 +1,13 @@
-import React, { useState } from "react";
-import TabNavItem from "./TabNavItem";
-import TabContent from "./TabContent";
-// Import our Map Components
+import React from "react";
 import OpenLayerComponent from "../MapComponents/OpenLayerComponent";
 
 
 const Tabs = () => {
-  const [activeTab, setActiveTab] = useState("openlayers");
 
   return (
     <div className="Tabs">
-      <ul className="nav">
-        <TabNavItem title="Web map GIS" id="openlayers" activeTab={activeTab} setActiveTab={setActiveTab} />
-      </ul>
-
       <div className="outlet">
-        <TabContent id="openlayers" activeTab={activeTab}>
-            <OpenLayerComponent></OpenLayerComponent>
-        </TabContent>
+      <OpenLayerComponent></OpenLayerComponent>
       </div>
     </div>
   );
